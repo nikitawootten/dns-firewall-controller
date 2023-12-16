6e9cf2025533ec6add86259330d2140cfcd58299
Nikita Wootten <nikita.wootten@gmail.com>
2023-12-16 10:51:44 -0500

    Minimal coredns plugin, closes #2

--- src/coredns_plugin/plugin.go
+++ src/coredns_plugin/plugin.go
@@ -1,12 +1,29 @@
 package squawker
 
 import (
+	"context"
+
 	"github.com/coredns/coredns/plugin"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/miekg/dns"
 )
 
-var log = clog.NewWithPlugin("squawker")
+var PLUGIN_NAME = "squawker"
+
+var log = clog.NewWithPlugin(PLUGIN_NAME)
 
 type Squawker struct {
 	Next plugin.Handler
 }
+
+func NewSquawker(next plugin.Handler) plugin.Handler {
+	return &Squawker{Next: next}
+}
+
+func (s *Squawker) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	log.Debug("Received response")
+	return plugin.NextOrFailure(s.Name(), s.Next, ctx, w, r)
+}
+
+// Name implements the Handler interface.
+func (s Squawker) Name() string { return PLUGIN_NAME }
--- src/coredns_plugin/setup.go
+++ src/coredns_plugin/setup.go
@@ -2,11 +2,18 @@ package squawker
 
 import (
 	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 )
 
 func init() { plugin.Register("squawker", setup) }
 
 func setup(c *caddy.Controller) error {
+	c.Next() // Ignore "squawker" and give us the next token.
+
+	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		return NewSquawker(next)
+	})
+
 	return nil
 }
